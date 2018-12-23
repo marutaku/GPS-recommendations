bee45bca7e2011078ee7381706c58a53a9762398
marutaku <satouredstone@gmail.com>
2018-12-24 00:07:25 +0900

    Pass recommend if keyerror happen

--- lib/models/Recommend.py
+++ lib/models/Recommend.py
@@ -17,41 +17,45 @@ class RecommendModel(object):
 
 
     def get_recommend(self, user_id, latitude, longitude, location_id):
-        cf = CollaborativeFilter(user_id)
-        cf_recommend = cf.get_recommend(user_id, -1)
-        cf_recommend_dict = {}
-        for item in cf_recommend:
-            key = list(item.keys())[0]
-            value = list(item.values())[0]
-            cf_recommend_dict[key] = {
-                'similarity': value
-            }
-        fq_recommend = self.fq.get_recommend_place(latitude, longitude)['response']['groups'][0]['items']
-        fq_recommend_dict = {}
+        try:
+            cf = CollaborativeFilter(user_id)
+            cf_recommend = cf.get_recommend(user_id, -1)
+            cf_recommend_dict = {}
+            for item in cf_recommend:
+                key = list(item.keys())[0]
+                value = list(item.values())[0]
+                cf_recommend_dict[key] = {
+                    'similarity': value
+                }
+            fq_recommend = self.fq.get_recommend_place(latitude, longitude)['response']['groups'][0]['items']
+            fq_recommend_dict = {}
 
-        for item in fq_recommend:
-            item = item['venue']
-            fq_recommend_dict[item['name']] = {
-                'latitude': item['location']['lat'],
-                'longitude': item['location']['lng']
-            }
+            for item in fq_recommend:
+                item = item['venue']
+                fq_recommend_dict[item['name']] = {
+                    'latitude': item['location']['lat'],
+                    'longitude': item['location']['lng']
+                }
 
-        # 各レコメンドの積集合を取得
-        fq_place_name_set = set(fq_recommend_dict.keys())
-        cf_place_name_set = set(cf_recommend_dict.keys())
-        recommend_candidate = fq_place_name_set.intersection(cf_place_name_set)
-        result_list = []
-        for i in recommend_candidate:
-            result_item = {i: cf_recommend_dict[i]}
-            result_list.append(result_item)
-        result_list.sort(key=lambda x: list(list(x.values())[0].values())[0], reverse=True)
-        if len(result_list) == 0: return None
-        recommend_item = result_list[0]
-        recommend_place_name = list(recommend_item.keys())[0]
-        place = self.place_db.get_place_by_name(recommend_place_name)[0]
-        self.recommend_db.insert_recommend_place(user_id, location_id, place.id)
-        recommend_item[recommend_place_name].update(fq_recommend_dict[recommend_place_name])
-        return recommend_item
+            # 各レコメンドの積集合を取得
+            fq_place_name_set = set(fq_recommend_dict.keys())
+            cf_place_name_set = set(cf_recommend_dict.keys())
+            recommend_candidate = fq_place_name_set.intersection(cf_place_name_set)
+            result_list = []
+            for i in recommend_candidate:
+                result_item = {i: cf_recommend_dict[i]}
+                result_list.append(result_item)
+            result_list.sort(key=lambda x: list(list(x.values())[0].values())[0], reverse=True)
+            if len(result_list) == 0: return None
+            recommend_item = result_list[0]
+            recommend_place_name = list(recommend_item.keys())[0]
+            place = self.place_db.get_place_by_name(recommend_place_name)[0]
+            self.recommend_db.insert_recommend_place(user_id, location_id, place.id)
+            recommend_item[recommend_place_name].update(fq_recommend_dict[recommend_place_name])
+            return recommend_item
+        except KeyError as e:
+            print(e)
+            return None
 
     def get_recommend_history(self, user_id):
         return self.recommend_db.get_recommend_history(user_id)
